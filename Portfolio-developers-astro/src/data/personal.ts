51f2def061fa3bd2143ad2e98aeea4bd44e1ec99
Aaditya Singh <a1946894@adelaide.edu.au>
2026-02-04 13:29:12 +0000

    Chagned resume

--- Portfolio-developers-astro/src/data/personal.ts
+++ Portfolio-developers-astro/src/data/personal.ts
@@ -4,7 +4,7 @@ export const PERSONAL_INFO = {
     journey: "My way of life involves trying and experiencing as many things as I possibly can. Picked up coding in high school, and since University started I have joined a startup in Adelaide, got top 10 in a competitive programming league, took part in Adelaide Rover Team and built some cool projects alongside!",
     skills: [""],
     heroDescription: "Scroll down to see more about me!",
-    resumeUrl: "https://violet-cyb-5.tiiny.site",
+    resumeUrl: "/latex7.pdf",
     socialLinks: [
         {
             href: "https://www.linkedin.com/in/aaditya-singh-a6918a332/",
